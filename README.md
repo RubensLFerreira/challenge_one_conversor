47d769666c2e81df593710ec3685288d62da5517
RubensLFerreira <rubenslima000@gmail.com>
2024-04-27 19:49:03 -0300

    criado a base do projeto

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-<div style="display: flex; flex-direction: column; align-items: center;">
-  <h1>Challenge ONE | Conversor de moedas</h1>
-  <img src="https://github.com/RubensLFerreira/challenge_one_conversor/assets/62750539/17b25857-5c28-4e11-a8a8-b1d92e452671" alt="Badge-Conversor">
-</div>
+ÿþ
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner prompt = new Scanner(System.in);
+
+    System.out.println("Seja bem-vindo(a) ao Conversor de Moeda\n");
+    String sair = "";
+
+    while (!sair.equalsIgnoreCase("sair")) {
+      System.out.println("Digite o código da moeda que deseja converte\n");
+      System.out.println("""
+          1) Dólar Americano (USD)
+          2) Peso Argentino (ARS)
+          3) Real brasileiro (RBL)
+          4) Peso colombiano (COP)
+          5) Yuan Chinesa (CNY)
+          6) Iene Japones (JPY)
+          7) Sair          
+          """);
+
+      System.out.println("Digite o código de qual moeda deseja converter: ");
+      String numberOne = prompt.nextLine();
+
+      if (numberOne.equalsIgnoreCase("7")) {
+        break;
+      }
+
+      System.out.println("\nDigite o valor que deseja converter: ");
+      String value = prompt.nextLine();
+
+      System.out.println("\nDigite o código para qual moeda deseja converter: ");
+      String numberTwo = prompt.nextLine();
+
+      if (numberTwo.equalsIgnoreCase("7")) {
+        break;
+      }
+
+      System.out.println("Primeiro código: " + numberOne + ", Valor: " + value + " Segundo código: " + numberTwo);
+    }
   }
 }
